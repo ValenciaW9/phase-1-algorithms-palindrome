77d7b96ecdb100e3ae70177a4538cd6de11599be
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-28 16:18:54 -0400

    phase-1-algorithms-palindrome/
    phase-1-algorithms-palindrome/

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
-function isPalindrome(word) {
-  // Convert the word to lowercase
-  word = word.toLowerCase();
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  let left = 0;
+  let right = str.length - 1;
 
-  // Reverse the word
-  let reversed = "";
-  for (let i = word.length - 1; i >= 0; i--) {
-    reversed += word[i];
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
   }
 
-  // Check if the reversed word is equal to the original word
-  return word === reversed;
+  return true;
 }
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+
+
+//function isPalindrome(string):
+// Convert the string to lowercase
+//lowercaseString = convertToLowercase(string)
+
+// Initialize pointers at the beginning and end of the string
+//eftPointer = 0
+//rightPointer = length(lowercaseString) - 1
+
+// Loop until the pointers cross each other
+//while leftPointer < rightPointer:
+  // If the characters at the pointers are not equal, return false
+  //if lowercaseString[leftPointer] is not equal to lowercaseString[rightPointer]:
+    //return false
+
+  // Move the pointers towards each other
+  //leftPointer = leftPointer + 1
+  //rightPointer = rightPointer - 1
+
+// If the loop completes without returning false, return true
+//return true
+
+
+  // If the loop completes without returning false, return true
+  //return true;
+}
+
 
 module.exports = isPalindrome;
 
@@ -19,26 +75,17 @@ console.log(isPalindrom("madam")); // Expectomg : true
 console.log(isPalindrom("robot")); //Expecting : false 
 
 /* 
+Pseudocoode: In the above pseudocode, we first convert the input string to lowercase using the convertToLowercase function (which is a placeholder for the actual implementation).
 
+Then, we initialize two pointers, leftPointer and rightPointer, pointing to the beginning and end of the string, respectively.
 
-//Pseudocoode: Convert the input word to lowercase to ignore case sensitivity.
-//Create an empty string called "reversed".
-//Loop through the characters of the word in reverse order.
-//Add each character to the "reversed" string.
-//Compare the "reversed" string with the original word.
-//if they are equal, return true, Otherwise, return false,
-//The code is clean and readable. Opitimize the present implementation is already optimal, as we only need to loop through the characters once to reverse the string.
+We enter a loop that continues until the pointers cross each other. Inside the loop, we compare the characters at the pointers. If they are not equal, we return false because the string is not a palindrome.
 
+If the characters at the pointers are equal, we move the leftPointer one step to the right and the rightPointer one step to the left.
 
-I want to write a function that determines if a given string is a palindrome. A palindrome is a word that reads the same forwards and  backwards.
-//In weiting your own test cases "madam" and "robot".  In thinking of a few additional test cases to cover different  scenarios:
-//Test case 1: "level" (Expecting:true)
-//Test case 2: "noon (Expecting: true)
-//Test case 3: "hello (Expecting: false)
-//Test case 4: "racecar" (Expecting: true)
-//Test case 5 "python" (Expecting : false )
+Once the loop completes without returning false, we can conclude that the string is a palindrome and return true.
 
-
+/*
 
 //Instructions:
 //Write a function  isPalindrome that will receive one argument, 
@@ -67,4 +114,6 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
 
