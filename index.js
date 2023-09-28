9afa45875efd7730d7c69da238b65f6d6f6bd6ca
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-28 16:42:17 -0400

    phase-1-algorithms-palindrome/
    phase-1-algorithms-palindrome/

--- index.js
+++ index.js
@@ -1,119 +1,48 @@
-function isPalindrome(str) {
-  str = str.toLowerCase();
-  let left = 0;
-  let right = str.length - 1;
+function isPalindrome(string) {
+  // Convert the string to lowercase
+  const lowercaseString = string.toLowerCase();
 
-  while (left < right) {
-    if (str[left] !== str[right]) {
+  // Initialize pointers at the beginning and end of the string
+  let leftPointer = 0;
+  let rightPointer = lowercaseString.length - 1;
+
+  // Loop until the pointers cross each other
+  while (leftPointer < rightPointer) {
+    // If the characters at the pointers are not equal, return false
+    if (lowercaseString[leftPointer] !== lowercaseString[rightPointer]) {
       return false;
     }
-    left++;
-    right--;
+
+    // Move the pointers towards each other
+    leftPointer++;
+    rightPointer--;
   }
 
+  // If the loop completes without returning false, return true
   return true;
 }
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
 module.exports = isPalindrome;
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-
-
-
+//Pseudocode
 //function isPalindrome(string):
-// Convert the string to lowercase
-//lowercaseString = convertToLowercase(string)
+  // Convert the string to lowercase
+  //lowercaseString = convertToLowercase(string)
 
-// Initialize pointers at the beginning and end of the string
-//eftPointer = 0
-//rightPointer = length(lowercaseString) - 1
+  // Initialize pointers at the beginning and end of the string
+  //leftPointer = 0
+  //rightPointer = lowercaseString.length - 1
 
-// Loop until the pointers cross each other
-//while leftPointer < rightPointer:
-  // If the characters at the pointers are not equal, return false
-  //if lowercaseString[leftPointer] is not equal to lowercaseString[rightPointer]:
-    //return false
+  // Loop until the pointers cross each other
+  //while leftPointer < rightPointer:
+    // If the characters at the pointers are not equal, return false
+    //if lowercaseString[leftPointer] is not equal to lowercaseString[rightPointer]:
+      //return false
 
-  // Move the pointers towards each other
-  //leftPointer = leftPointer + 1
-  //rightPointer = rightPointer - 1
-
-// If the loop completes without returning false, return true
-//return true
-
+    // Move the pointers towards each other
+    //leftPointer = leftPointer + 1
+    //rightPointer = rightPointer - 1
 
   // If the loop completes without returning false, return true
-  //return true;
-}
-
-
-module.exports = isPalindrome;
-
-//Test Cases
-console.log(isPalindrom("madam")); // Expectomg : true
-console.log(isPalindrom("robot")); //Expecting : false 
-
-/* 
-Pseudocoode: In the above pseudocode, we first convert the input string to lowercase using the convertToLowercase function (which is a placeholder for the actual implementation).
-
-Then, we initialize two pointers, leftPointer and rightPointer, pointing to the beginning and end of the string, respectively.
-
-We enter a loop that continues until the pointers cross each other. Inside the loop, we compare the characters at the pointers. If they are not equal, we return false because the string is not a palindrome.
-
-If the characters at the pointers are equal, we move the leftPointer one step to the right and the rightPointer one step to the left.
-
-Once the loop completes without returning false, we can conclude that the string is a palindrome and return true.
-
-/*
-
-//Instructions:
-//Write a function  isPalindrome that will receive one argument, 
-//a string. Your function should return true if the string is a palindroms. 
-//(that is, if it reads the same forwards and backwards, 
-//like "mom" or "raceccar"), and return false if it is not a palindrome.
-
-
-
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
-
-
-
+  //return true
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,21 +1,24 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const isPalindrome = require("../index");
+const isPalindrome = require('../index.js');
+const assert = require('assert');
 
-describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
+describe('isPalindrome', () => {
+  it('returns true for "abba"', () => {
+    assert.strictEqual(isPalindrome('abba'), true);
   });
-  it("returns true for 'racecar'", () => {
-    expect(isPalindrome("racecar")).to.be.true;
+
+  it('returns true for "racecar"', () => {
+    assert.strictEqual(isPalindrome('racecar'), true);
   });
-  it("returns true for 'a'", () => {
-    expect(isPalindrome("a")).to.be.true;
+
+  it('returns true for "a"', () => {
+    assert.strictEqual(isPalindrome('a'), true);
   });
-  it("returns false for 'robot'", () => {
-    expect(isPalindrome("robot")).to.be.false;
+
+  it('returns false for "robot"', () => {
+    assert.strictEqual(isPalindrome('robot'), false);
   });
-  it("returns false for 'ab'", () => {
-    expect(isPalindrome("ab")).to.be.false;
+
+  it('returns false for "ab"', () => {
+    assert.strictEqual(isPalindrome('ab'), false);
   });
 });
